30542aa9eb1f61048f1b28182ba2c186f02dc833
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-23 18:47:31 -0300

    Arquivo script_calculo.js - Adicionado a const para anoAtual, para facilitar o calculo da idade do veiculo, modificado a forma que os if estavam escritos agora objVeiculo.objliteral esta dentro de um paretese para receber o .toFixed(2), o primeiro if do calculoFinal foi modificado para (parseFloat(calculoIdade(objVeiculo)) > 20), as comparações dos objVeiculo em todos os calculos estava assim ex: ((objVeiculo.comb) = eta o correto é === ao inves de apenas um sinal = e por ultimo o import dos calculos foi disposto com os calculos em um unico import

--- js/script_calculo.js
+++ js/script_calculo.js
@@ -1,21 +1,24 @@
-/*O seguro será fixado em 10% do
+/*
+O seguro será fixado em 10% do
 valor do veículo para todos os casos, enquanto o IPVA será
 calculado de acordo com o tipo de combustível: veículos a
 gasolina pagam 20%, etanol 15%, bicombustíveis 10%, híbridos
 8% e elétricos 2%. Caso o veículo tenha mais de 20 anos de
-fabricação, ele será considerado isento de IPVA,*/
+fabricação, ele será considerado isento de IPVA, sendo
+exibido esse status na listagem.
+*/
 const calculoIpva = (objVeiculo) =>{
     let resposta = ''
 
     if((2026 - parseFloat(objVeiculo.anofab)) > 20){
         resposta = `Isento de IPVA`
-    }else if((objVeiculo.comb) = `gas`){
+    }else if((objVeiculo.comb) === `gas`){
         resposta = `R$ ${(parseFloat(objVeiculo.valmerc) * 0.2).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `eta`){
+    }else if((objVeiculo.comb) === `eta`){
         resposta = `R$ ${(parseFloat(objVeiculo.valmerc) * 0.15).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `bio`){
+    }else if((objVeiculo.comb) === `bio`){
         resposta = `R$ ${(parseFloat(objVeiculo.valmerc) * 0.1).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = 'hibr'){
+    }else if((objVeiculo.comb) === 'hibr'){
         resposta = `R$ ${(parseFloat(objVeiculo.valmerc) * 0.08).toFixed(2).replace('.',',')}`
     }else{
         resposta = `R$ ${(parseFloat(objVeiculo.valmerc) * 0.02).toFixed(2).replace('.',',')}`
@@ -23,8 +26,6 @@ const calculoIpva = (objVeiculo) =>{
 
     return resposta
 }
-
-export {calculoIpva}
 
 const calculoSeg = (objVeiculo) =>{
     let resposta = ''
@@ -34,36 +35,35 @@ const calculoSeg = (objVeiculo) =>{
     return resposta
 }
 
-export {calculoSeg}
+const anoAtual = new Date().getFullYear()
 
 const calculoIdade = (objVeiculo) =>{
     let resposta = ''
 
-    resposta = ` ${(2026 - parseFloat(objVeiculo.anofab))} `
+    resposta = `${anoAtual - parseFloat(objVeiculo.anofab)}`
 
     return resposta
 }
 
-export {calculoIdade}
+
 
 const calculoFinal = (objVeiculo) =>{
     let resposta = ''
 
-    if((2026 - (objVeiculo.anofab)) > 20){
+    if(parseFloat(calculoIdade(objVeiculo)) > 20){
         resposta = `R$ ${(parseFloat(objVeiculo.valmerc) * 0.1).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `gas`){
+    }else if((objVeiculo.comb) === `gas`){
         resposta = `R$ ${((parseFloat(objVeiculo.valmerc) * 0.2) + (parseFloat(objVeiculo.valmerc) * 0.1)).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `eta`){
+    }else if((objVeiculo.comb) === `eta`){
         resposta = `R$ ${((parseFloat(objVeiculo.valmerc) * 0.15) + (parseFloat(objVeiculo.valmerc) * 0.1)).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `bio`){
+    }else if((objVeiculo.comb) === `bio`){
         resposta = `R$ ${((parseFloat(objVeiculo.valmerc) * 0.1) + (parseFloat(objVeiculo.valmerc) * 0.1)).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `hibr`){
+    }else if((objVeiculo.comb) === `hibr`){
         resposta = `R$ ${((parseFloat(objVeiculo.valmerc) * 0.08) + (parseFloat(objVeiculo.valmerc) * 0.1)).toFixed(2).replace('.',',')}`
-    }else if((objVeiculo.comb) = `elet`){
+    }else if((objVeiculo.comb) === `elet`){
         resposta = `R$ ${((parseFloat(objVeiculo.valmerc) * 0.02) + (parseFloat(objVeiculo.valmerc) * 0.1)).toFixed(2).replace('.',',')}`
     }
-
     return resposta
 }
 
-export {calculoFinal}
+export { calculoIpva, calculoSeg, calculoIdade, calculoFinal }
